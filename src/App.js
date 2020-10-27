a2a6d2cd9a8134a9da4c069e03976aed652b0d5b
ardani13 <sulistyoardani@gmail.com>
2020-10-27 15:17:04 +0700

    first push

--- src/App.js
+++ src/App.js
@@ -12,22 +12,30 @@ class App extends Component {
         hobi : 'ngentu bojone uwong'
       }
     ],
-    otherState : 'duarr'
+    otherState : 'duarr',
+    personStat : true,
   }
-
+  
   gantiHobiHandler = (duar) => {
-    console.log('This Clicked');
+    console.log('This Clicked', duar);
     // this.state.hobbies[0].hobi = 'Ihik dong'
-    this.setState({
-      hobbies: [
-        {
-          hobi: 'gelut ayo'
-        },
-        {
-          hobi: duar
-        }
-      ]
-    });
+    if(duar == 'Mancikk i bojomu') {
+      this.setState({
+        hobbies: [
+          {
+            hobi: 'gelut ayo'
+          },
+          {
+            hobi: duar
+          }
+        ]
+      });
+    }
+    else {
+      this.setState({
+        personStat: !this.state.personStat
+      })
+    }
   }
 
   gantiHobiInput = (event) => {
@@ -51,6 +59,18 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     }
+
+    let persons = null;
+
+    if(this.state.personStat) {
+      persons = (
+        <div>
+          <Person hobi={this.gantiHobiInput} hobbies={this.state.hobbies[0].hobi}/>
+        <Person click={this.gantiHobiHandler.bind(this, 'Mancing')} hobbies="kentu">jancuk</Person>
+        <Person hobbies={this.state.hobbies[1].hobi}/>
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>tes</h1>
@@ -58,11 +78,9 @@ class App extends Component {
         {/* <button onClick={this.gantiHobiHandler.bind(this, 'Mancik')}>Ganti Hobi</button> */}
         {/* using arrow function */}
 
-        <button style={style} onClick={() => this.gantiHobiHandler('Mancikk i bojomu')}>Ganti Hobi</button>
+        <button style={style} onClick={() => this.gantiHobiHandler('Mancikk i bojom')}>Ganti Hobi</button>
         {/* hobbies is a props */}
-        <Person hobi={this.gantiHobiInput} hobbies={this.state.hobbies[0].hobi}/>
-        <Person click={this.gantiHobiHandler.bind(this, 'Mancing')} hobbies="kentu">jancuk</Person>
-        <Person hobbies={this.state.hobbies[1].hobi}/>
+      {persons}
 
 
         {/* <header className="App-header">
